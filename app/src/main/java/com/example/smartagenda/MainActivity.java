a51e476b04e67a167f4fef849340a73471d91f20
penelope <penelope.rekkas@student.kuleuven.be>
2022-04-24 20:15:46 +0200

    update 24/04

--- app/src/main/java/com/example/smartagenda/MainActivity.java
+++ app/src/main/java/com/example/smartagenda/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.smartagenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button logInBtn;
+    private Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //logInBtn = (Button) findViewById(R.id.logInBtn);
+        //signUpBtn = (Button) findViewById(R.id.signUpBtn);
+    }
+
+
+    public void onSignUpBtn_Clicked(View caller)
+    {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    public void onLogInBtn_Clicked(View caller)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/smartagenda/SignUpActivity.java
@@ -0,0 +1,64 @@
+package com.example.smartagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText email;
+    private EditText password;
+    private EditText confirmPassword;
+    private Button save;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        username = (EditText) findViewById(R.id.usernameIn);
+        email = (EditText) findViewById(R.id.emailIn);
+        password = (EditText) findViewById(R.id.passwordIn);
+        confirmPassword = (EditText) findViewById(R.id.password2In);
+
+        save = (Button) findViewById(R.id.saveBtn1);
+
+    }
+
+    public void onSave1Btn_Clicked(View caller)
+    {
+
+        if (!username.getText().toString().matches("")
+                && !email.getText().toString().matches("")
+                &&!password.getText().toString().matches("")
+                &&!confirmPassword.getText().toString().matches("")
+                && password.getText().toString().equals(confirmPassword.getText().toString()))
+        {
+            Intent intent = new Intent(this, PersonalActivity.class);
+            startActivity(intent);
+        }
+        else
+        {
+            if (username.getText().toString().matches("")
+                    || email.getText().toString().matches("")
+                    || password.getText().toString().matches("")
+                    || confirmPassword.getText().toString().matches(""))
+            {
+                Toast.makeText(this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+            }
+            if (!password.getText().toString().equals(confirmPassword.getText().toString()))
+            {
+                Toast.makeText(this, "Passwords don't match", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+
+    }
+}
